72efdd192e3373a16cfaf323b08fc964c8f9b881
Dawid Mariankowski <dawid24.9@o2.pl>
2018-10-10 11:28:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-<script>
+
+    
+/*
+    {
+    const clasesAccordion = document.querySelectorAll('.gs-faq__question');
+    for(let = i; i<clasesAccordion.length; i++){
+document.querySelectorAll("[id^="tab"]").addEventListener("click", function(event) {
+}
+
+    clasesAccordion.forEach((item) => item.addEventListener
+             ("click", event))
+        if(clasesAccordion[i].style.display == 'block'){
+            continue;
+        }else{
+document.clasesAccordion[i].style.display = 'none';
+        }
+
+
+}           
+event.preventDefault();
+}, false);
+    }
+*/
+    
+
+    <script>
 
 document.addEventListener("DOMContentLoaded", function() {
 
 
 (function () { 
     let AccordionEl   = document.querySelectorAll('[id*="tab"]');
+for(let i=0; i<AccordionEl.length;i++){
+document.querySelector("#tab"+[i]+1+"-0").addEventListener("click", function(event) {
     
-
-    
-for(let=1; i<AccordionEl.length;
-document.querySelector("#tab1-0").addEventListener("click", function(event) {
-    
-
-     for(var i =0; i<=5; i++){
-                if(document.getElementsByClassName('gs-faq__answer')[0].style.display == 'block'){
+}
+                                                           }
+     for(var i =0; i<AccordionEl.lengt; i++){
+                if(document.getElementsByClassName('gs-faq__answer')[i].style.display == 'block'){
                     continue;
                 }else{
         document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
